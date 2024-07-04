1150531d4b7379845b0ed61f60f47464a18e0fd2
Sofia <sofia.sarotri24@gmail.com>
2024-07-04 14:57:40 -0300

    setting up the folder structure

---
+++ Tibby/Tibby/Views/HomeView/Store/StoreView.swift
@@ -0,0 +1,18 @@
+//
+//  StoreView.swift
+//  Tibby
+//
+//  Created by Sofia Sartori on 04/07/24.
+//
+
+import SwiftUI
+
+struct StoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreView()
+}
---
+++ Tibby/Tibby/Views/Onboarding/Onboarding.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding.swift
+//  Tibby
+//
+//  Created by Sofia Sartori on 04/07/24.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Onboarding()
+}
