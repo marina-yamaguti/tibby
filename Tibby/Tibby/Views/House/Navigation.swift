d1907fbd0c0cb9ed9f69d004a42025322b6afa44
felipeelsnersilva15@gmail.com <Felipe Elsner Silva>
2024-07-15 16:23:18 -0300

    feat: create navigation

---
+++ Tibby/Tibby/Views/House/Navigation.swift
@@ -0,0 +1,71 @@
+//
+//  Navigation.swift
+//  Tibby
+//
+//  Created by Felipe  Elsner Silva on 15/07/24.
+//
+
+import SwiftUI
+//MARK: Types of enviroments
+enum Enviroment {
+    case garden, kitchen, bedroom
+    
+    func getIconAsset() -> String {
+        switch self {
+        case .garden:
+            return "tree.fill"
+        case .kitchen:
+            return "fork.knife"
+        case .bedroom:
+            return "bed.double.fill"
+        }
+    }
+    
+//    func getbackground() -> String {
+//        switch self {
+//        case .garden:
+//            return ""
+//        case .kitchen:
+//            return ""
+//        case .bedroom:
+//            return ""
+//        }
+//    }
+}
+
+struct NavigationTabbarView: View {
+    @EnvironmentObject var constants: Constants
+    let enviroments: [Enviroment] = [.kitchen, .bedroom, .garden]
+    
+    var body: some View {
+        VStack {
+            //Decide the room depending on the button the user select
+            switch constants.currentEnviroment {
+            case .bedroom:
+                BedroomView()
+                    .brightness(constants.brightness)
+            case .garden:
+                GardenView()
+                    .brightness(constants.brightness)
+            case .kitchen:
+                KitchenView()
+                    .brightness(constants.brightness)
+            }
+            //Custom Tabbar
+            HStack(spacing: 30){
+                ForEach(0..<enviroments.count) { ind in
+                    Button {
+                        constants.currentEnviroment = enviroments[ind]
+                    } label: {
+                        Image(systemName: enviroments[ind].getIconAsset())
+                    }
+                }
+            }
+        }
+        .background(
+        
+        )
+    }
+
+    
+}
