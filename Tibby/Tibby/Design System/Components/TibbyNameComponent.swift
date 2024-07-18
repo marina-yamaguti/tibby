542fd4c065f9435a90e7897801c3baf5a97e4813
Natalia Dal Pizzol <nataliadpizzol@gmail.com>
2024-07-18 16:34:27 -0300

    Updating folders

---
+++ Tibby/Tibby/Design System/Components/TibbyNameComponent.swift
@@ -0,0 +1,26 @@
+//
+//  TibbyNameComponent.swift
+//  Tibby
+//
+//  Created by Natalia Dal Pizzol on 18/07/24.
+//
+
+import SwiftUI
+
+struct TibbyNameComponent: View {
+    @State var name: String
+    var body: some View {
+        HStack {
+            Text(name)
+                .font(.typography(.body))
+                .padding(EdgeInsets(top: 8, leading: 20, bottom: 8, trailing: 20))
+                .foregroundStyle(Color.tibbyBaseBlack)
+        }
+        .background(Color.tibbyBaseWhite.opacity(0.5))
+        .withBorderRadius(20)
+    }
+}
+
+#Preview {
+    TibbyNameComponent(name: "Everton")
+}
