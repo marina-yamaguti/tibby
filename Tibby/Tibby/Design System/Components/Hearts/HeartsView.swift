3b5d4011009403178700aabba14f7482d717bb1c
marina-yamaguti <marina.geller.yamaguti@gmail.com>
2024-07-18 16:58:03 -0300

    fix: tibby logic

--- Tibby/Tibby/Design System/Components/Hearts/HeartsView.swift
+++ Tibby/Tibby/Design System/Components/Hearts/HeartsView.swift
@@ -3,31 +3,31 @@
 //  Tibby
 //
 //  Created by Marina Yamaguti on 18/07/24.
-//
 
-//import SwiftUI
-//
-//struct HeartsView: View {
-//    @ObservedObject var viewModel: HeartsViewModel
-//    
-//    var body: some View {
-//        VStack(alignment: .center, spacing: 8) {
-//            HStack(spacing: 8) {
-//                ForEach(0..<3) { index in
-//                    Image(viewModel.getHeartImage(index: index))
-//                        .resizable()
-//                        .frame(width: 50, height: 50)
-//                }
-//            }
-//            Text(viewModel.category.rawValue)
-//                .font(.typography(.title))
-//        }
-//        .onAppear {
-//            viewModel.fetchCategoryValue()
-//        }
-//    }
+
+import SwiftUI
+
+struct HeartsView: View {
+    @ObservedObject var viewModel: HeartsViewModel
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 8) {
+            HStack(spacing: 8) {
+                ForEach(0..<3) { index in
+                    Image(viewModel.getHeartImage(index: index))
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                }
+            }
+            Text(viewModel.category.rawValue)
+                .font(.typography(.title))
+        }
+        .onAppear {
+            viewModel.fetchCategoryValue()
+        }
+    }
+}
+
+//#Preview {
+//HeartsView(viewModel: HeartsViewModel(category: .hunger, tibby: Tibby(id: UUID(), hunger: 50, sleep: 70, happiness: 90), service: Service()))
 //}
-//
-////#Preview {
-////    HeartsView(viewModel: HeartsViewModel(category: .hunger, service: Service()))
-////}
