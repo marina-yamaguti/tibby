7319b05281944bc3c7a3d2c99cc36808bed7316a
marina-yamaguti <marina.geller.yamaguti@gmail.com>
2024-07-18 15:57:54 -0300

    fix: commenting errors to commit

--- Tibby/Tibby/Design System/Components/Hearts/HeartsView.swift
+++ Tibby/Tibby/Design System/Components/Hearts/HeartsView.swift
@@ -5,29 +5,29 @@
 //  Created by Marina Yamaguti on 18/07/24.
 //
 
-import SwiftUI
-
-struct HeartsView: View {
-    @ObservedObject var viewModel: HeartsViewModel
-    
-    var body: some View {
-        VStack(alignment: .center, spacing: 8) {
-            HStack(spacing: 8) {
-                ForEach(0..<3) { index in
-                    Image(viewModel.getHeartImage(index: index))
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                }
-            }
-            Text(viewModel.category.rawValue)
-                .font(.typography(.title))
-        }
-        .onAppear {
-            viewModel.fetchCategoryValue()
-        }
-    }
-}
-
-//#Preview {
-//    HeartsView(viewModel: HeartsViewModel(category: .hunger, service: Service()))
+//import SwiftUI
+//
+//struct HeartsView: View {
+//    @ObservedObject var viewModel: HeartsViewModel
+//    
+//    var body: some View {
+//        VStack(alignment: .center, spacing: 8) {
+//            HStack(spacing: 8) {
+//                ForEach(0..<3) { index in
+//                    Image(viewModel.getHeartImage(index: index))
+//                        .resizable()
+//                        .frame(width: 50, height: 50)
+//                }
+//            }
+//            Text(viewModel.category.rawValue)
+//                .font(.typography(.title))
+//        }
+//        .onAppear {
+//            viewModel.fetchCategoryValue()
+//        }
+//    }
 //}
+//
+////#Preview {
+////    HeartsView(viewModel: HeartsViewModel(category: .hunger, service: Service()))
+////}
