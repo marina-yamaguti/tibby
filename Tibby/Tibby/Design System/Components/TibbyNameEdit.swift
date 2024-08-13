c78088ec374942ef0b3590b11f18d2a1b8bbe62a
marina-yamaguti <marina.geller.yamaguti@gmail.com>
2024-08-13 19:20:44 -0300

    feat: components

--- Tibby/Tibby/Design System/Components/TibbyNameEdit.swift
+++ Tibby/Tibby/Design System/Components/TibbyNameEdit.swift
@@ -22,30 +22,56 @@ struct TibbyNameEdit: View {
                             .onAppear {
                                 textWidth = geometry.size.width
                             }
-                            .onChange(of: name) { oldValue, newValue in
+                            .onChange(of: name) { _, _ in
                                 textWidth = geometry.size.width
                             }
                     })
                     .hidden()
                 
-                TextField(
-                    "Given Name",
-                    text: $name
-                )
-                .disableAutocorrection(true)
-                .font(.typography(FontStyle.headline))
-                .foregroundStyle(Color.tibbyBaseBlack)
-                .frame(width: textWidth)
+                if isEditing {
+                    TextField(
+                        "Given Name",
+                        text: $name
+                    )
+                    .disableAutocorrection(true)
+                    .font(.typography(FontStyle.headline))
+                    .foregroundStyle(Color.tibbyBaseBlack)
+                    .frame(width: textWidth)
+                    .onChange(of: name) { _, newValue in
+                        if newValue.count > 10 {
+                            name = String(newValue.prefix(10))
+                        }
+                    }
+                    .onSubmit {
+                        isEditing = false
+                    }
+                } else {
+                    Text(name)
+                        .font(.typography(FontStyle.headline))
+                        .foregroundStyle(Color.tibbyBaseBlack)
+                        .frame(width: textWidth)
+                        .onTapGesture {
+                            isEditing = true
+                        }
+                }
             }
             
+            //icon
             ZStack {
-                Circle().foregroundStyle(.black.opacity(0.5))
-                Image(systemName: "pencil")
+                Circle().foregroundStyle(isEditing ? Color.tibbyBaseGreen : .black.opacity(0.5))
+                Image(systemName: isEditing ? "checkmark" : "pencil")
                     .resizable()
                     .frame(width: 7, height: 7)
                     .foregroundStyle(Color.tibbyBaseWhite)
             }
             .frame(width: 20, height: 20)
+            .onTapGesture {
+                if isEditing {
+                    isEditing = false
+                } else {
+                    isEditing = true
+                }
+            }
         }.frame(height: 36)
     }
 }
