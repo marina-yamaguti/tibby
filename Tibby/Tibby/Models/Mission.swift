f0af453e30339bfc1d04edf1b1ce933657562be0
felipeelsnersilva15@gmail.com <Felipe Elsner Silva>
2024-08-29 17:06:04 -0300

    feat: create mission protocol

---
+++ Tibby/Tibby/Models/Mission.swift
@@ -0,0 +1,98 @@
+//
+//  Mission.swift
+//  Tibby
+//
+//  Created by Felipe  Elsner Silva on 29/08/24.
+//
+
+import Foundation
+
+/// Represents the progress of a mission.
+///
+/// - progress: The mission is currently in progress.
+/// - claim: The mission is completed and the reward can be claimed.
+/// - done: The mission is fully completed and no further actions are needed.
+enum MissionProgress {
+    case progress, claim, done
+}
+
+/// Represents different types of missions that can be assigned.
+///
+/// - workout: A mission related to physical exercise.
+/// - feed: A mission related to feeding.
+/// - sleep: A mission related to sleep.
+/// - steps: A mission related to taking steps.
+enum MissionType {
+    case workout, feed, sleep, steps
+    
+    /// The icon associated with each mission type.
+    var icon: String {
+        switch self {
+        case .workout:
+            return "TibbySymbolDumbbell"
+        case .feed:
+            return "TibbySymbolCarrot"
+        case .sleep:
+            return "TibbySymbolSleep"
+        case .steps:
+            return "TibbySymbolSteps"
+        }
+    }
+}
+
+/// A protocol defining the required properties and methods for a mission.
+protocol MissionProtocol {
+    /// A description of the mission.
+    var description: String { get }
+    
+    /// The total value or goal of the mission.
+    var valueTotal: Int { get set }
+    
+    /// The value or progress that has been completed so far.
+    var valueDone: Int { get set }
+    
+    /// The reward given upon completion of the mission.
+    var reward: Reward { get }
+    
+    /// The frequency or timing for the mission.
+    var frequencyTime: DateType { get }
+    
+    /// The current progress status of the mission.
+    var progress: MissionProgress { get set }
+    
+    /// The type of mission.
+    var missionType: MissionType { get }
+    
+    /// Claims the reward for the mission.
+    ///
+    /// - Parameters:
+    ///   - user: The user claiming the reward.
+    ///   - tibby: The Tibby associated with the mission.
+    func claimReward(user: User?, tibby: Tibby?)
+    
+    /// Updates the progress of the mission.
+    ///
+    /// - Parameter value: The new value to update the progress by.
+    func updateProgress(value: Int)
+}
+
+/// A protocol defining a collection of missions.
+protocol MissionsCollectionProtocol {
+    /// The frequency or timing for the entire collection of missions.
+    var frequencyTime: DateType { get }
+    
+    /// The title of the mission collection.
+    var title: String { get }
+    
+    /// The list of missions included in the collection.
+    var missions: [MissionProtocol] { get set }
+    
+    /// The remaining time for the collection of missions.
+    var timeRemaning: (timeValue: Int, timeMesure: TimeMesure) { get set }
+    
+    /// Sets the remaining time for the missions in the collection.
+    func setTimeRemaning()
+    
+    /// Creates the missions included in the collection.
+    func createMissions()
+}
--- Tibby/Tibby/Models/Reward.swift
+++ Tibby/Tibby/Models/Reward.swift
@@ -18,51 +18,53 @@ enum RewardType {
 ///
 /// This protocol outlines methods for granting rewards to users or Tibbies and for leveling up Tibbies.
 protocol RewardProtocol {
+    /// Parameters that all Reward needs to have
+    var rewardValue: Int { get }
+    var rewardType: RewardType { get }
     
     /// Grants a reward of the specified type and quantity to a user or Tibby.
     ///
     /// - Parameters:
-    ///   - quantity: The amount of the reward to be granted.
-    ///   - rewardType: The type of reward to be granted (`xp`, `coin`, or `gem`).
     ///   - user: An optional `User` object to receive the reward (for coins or gems).
     ///   - tibby: An optional `Tibby` object to receive the reward (for XP).
-    func reward(quantity: Int, rewardType: RewardType, user: User?, tibby: Tibby?)
-    
-    /// Levels up the specified Tibby if its XP meets the criteria.
-    ///
-    /// - Parameter tibby: The `Tibby` to be leveled up.
-    func levelUp(_ tibby: Tibby)
+    func reward(user: User?, tibby: Tibby?)
 }
 
 /// A class that implements the `RewardProtocol`, providing functionality to manage rewards and Tibby leveling.
 class Reward: RewardProtocol {
+    /// Parameters of the value of the reward that the user will claim and the type of it
+    var rewardValue: Int
+    var rewardType: RewardType
+    
+    init(rewardValue: Int, rewardType: RewardType) {
+        self.rewardValue = rewardValue
+        self.rewardType = rewardType
+    }
     
     /// Grants a reward of the specified type and quantity to a user or Tibby.
     ///
     /// Depending on the `rewardType`, this method will either increase the Tibby's XP, the user's coins, or the user's gems.
     ///
     /// - Parameters:
-    ///   - quantity: The amount of the reward to be granted.
-    ///   - rewardType: The type of reward to be granted (`xp`, `coin`, or `gem`).
     ///   - user: An optional `User` object to receive the reward (for coins or gems).
     ///   - tibby: An optional `Tibby` object to receive the reward (for XP).
-    func reward(quantity: Int, rewardType: RewardType, user: User? = nil, tibby: Tibby? = nil) {
+    func reward(user: User? = nil, tibby: Tibby? = nil) {
         switch rewardType {
         case .xp:
             // Increase the Tibby's XP and check for level up
             if let tibby = tibby {
-                tibby.xp += quantity
+                tibby.xp += rewardValue
                 levelUp(tibby)
             }
         case .coin:
             // Increase the user's coins
             if let user = user {
-                user.coins += quantity
+                user.coins += rewardValue
             }
         case .gem:
             // Increase the user's gems
             if let user = user {
-                user.gems += quantity
+                user.gems += rewardValue
             }
         }
     }
@@ -72,7 +74,7 @@ class Reward: RewardProtocol {
     /// The Tibby will level up if its XP exceeds the required amount, with any excess XP carried over to the next level.
     ///
     /// - Parameter tibby: The `Tibby` to be leveled up.
-    internal func levelUp(_ tibby: Tibby) {
+    private func levelUp(_ tibby: Tibby) {
         // Check if the Tibby's XP meets the threshold for leveling up
         if Constants.singleton.maxLevel == -1 || tibby.level < Constants.singleton.maxLevel {
             let xpToEvolve = tibby.level * Constants.singleton.xpPerLevel
